7ef262151c7ad97a2a370a8b94cacc2a4037cf46
TienPhamDev <manhtienbn18891@gmail.com>
2022-12-08 21:31:28 -0500

    code

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+"use strict";
+const navEl = document.querySelector(".nav");
+const darkscreenEl = document.querySelector(".dark-sceen");
+const menuBtnOpenEl = document.querySelector(".menu-btn-open");
+const menuBtnCloseEl = document.querySelector(".menu-btn-close");
+const menuBtnEl = document.querySelectorAll(".menu-btn");
+let mediaScreen1440 = window.matchMedia("(min-width:1440px)");
+
+for (let i = 0; i < menuBtnEl.length; i++) {
+  menuBtnEl[i].addEventListener("click", function () {
+    navEl.classList.toggle("hidden");
+    darkscreenEl.classList.toggle("hidden");
+    menuBtnCloseEl.classList.toggle("hidden");
+    menuBtnOpenEl.classList.toggle("hidden");
+  });
+}
+function screen1440() {
+  if (mediaScreen1440.matches) {
+    navEl.classList.remove("hidden");
+    menuBtnOpenEl.classList.add("hidden");
+  } else {
+    navEl.classList.add("hidden");
+    menuBtnOpenEl.classList.remove("hidden");
+  }
+}
+screen1440(mediaScreen1440);
+mediaScreen1440.addListener(screen1440);
